ee8b1076f4ee198732fcbb5a987b6e22d0bb3a61
GUL <guillaume@hey-gul.com>
2022-01-22 12:53:07 -0500

    finalize demo related to untracked files

--- demos/1-untracked-files.sh
+++ demos/1-untracked-files.sh
@@ -12,26 +12,29 @@ if [ -d "remote-repo/.git" ]; then
 fi
 
 cp -r remote-repo/git remote-repo/.git
-# clone the repo from local
 
+# clone the repo from local
 mkdir local-repo
 git clone --local remote-repo local-repo
 cd local-repo
 
 # switch to Roger's branch
-git switch a-kind-of-magic
+git switch -c a-kind-of-magic
 
 # copy new files from hidden folder
-cp ./albums/1985_xx_yy_A_Kind_Of_Magic/A_Kind_Of_Magic/.new-files/*.md ./albums/1985_xx_yy_A_Kind_Of_Magic/A_Kind_Of_Magic/
+mkdir -p ./albums/1985_xx_yy_A_Kind_Of_Magic/A_Kind_Of_Magic/
+cp ../remote-repo/albums/1985_xx_yy_A_Kind_Of_Magic/A_Kind_Of_Magic/.new-files/*.md ./albums/1985_xx_yy_A_Kind_Of_Magic/A_Kind_Of_Magic/
 
 # change git user to Roger Taylor
 git config user.name "Roger Taylor"
 git config user.email roger@queenonline.com
 
 # demo untracked files management
+pe "git status"
 pe "git status --untracked-files=all"
 pe "git add --interactive"
 pe "git commit"
+pe "git push -u origin a-kind-of-magic"
 
 # enters interactive mode and allows newly typed command to be executed
 # cmd
